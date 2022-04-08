9ac7bd902995fc53e3b7b7a1bd63bc96c559e16e
徐梦强 <youxiubiji@gmail.com>
2022-04-08 15:53:00 +0800

    feat：添加eslint格式化校验

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true, // browser global variables
+    es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+  },
+  parser: 'vue-eslint-parser',
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended', // ++  放置在extends数组的最后
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  globals: {
+    defineProps: 'readonly',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
